23b76256e5375a7e3ba6b44d3511d43309a1bff9
Michael O'Dell <69868930+MikeOD92@users.noreply.github.com>
2021-09-09 22:34:48 +0000

    reduced create function to much smaller loop through the sheet object.

--- main.py
+++ main.py
@@ -45,31 +45,32 @@ class MyClient(discord.Client):
     
     msg = message.content
 
-    if msg.startswith('/hey'):
-      await message.channel.send(f'Hello it is I {self.user.name}')
+    # if msg.startswith('/hey'):
+    #   await message.channel.send(f'Hello it is I {self.user.name}')
     
-    if msg.startswith('/roll'):
-      roll = msg.split('/roll', 1)[1]
+    # if msg.startswith('/roll'):
+    #   roll = msg.split('/roll', 1)[1]
 
-      if(roll.split('d')[0] == " " or roll.split('d')[0] == "" ):
-        num = 1
-      else:
-        num = int(roll.split('d')[0])
+    #   if(roll.split('d')[0] == " " or roll.split('d')[0] == "" ):
+    #     num = 1
+    #   else:
+    #     num = int(roll.split('d')[0])
 
-      sides = int(roll.split('d')[1])
+    #   sides = int(roll.split('d')[1])
 
-      await message.channel.send(str(message.author) + " rolled " +str(roll))
+    #   await message.channel.send(str(message.author) + " rolled " +str(roll))
       
-      total = 0
+    #   total = 0
 
-      while num > 0:
-        rolled_num = random.randint(1,sides)
-        total = rolled_num + total
-        await message.channel.send(rolled_num)
-        num -= 1
+    #   while num > 0:
+    #     rolled_num = random.randint(1,sides)
+    #     total = rolled_num + total
+    #     await message.channel.send(rolled_num)
+    #     num -= 1
 
-      await message.channel.send('total val is ' + str(total))  
-      return total
+    #   await message.channel.send('total val is ' + str(total))  
+    #   return total
+
 ### build character sheet with charsheet class
     if msg.startswith('/create-char'):
 
@@ -89,69 +90,27 @@ class MyClient(discord.Client):
 
 
       await message.channel.send('Hello Travler')
-      await message.channel.send('What is your name ?')
-      name = await client.wait_for('message')
 
-      player_sheet["name"] = name.content
+      for i in player_sheet:
+        if i == "name":
+          await message.channel.send('What is your name ?')
+          name = await client.wait_for('message')
+          player_sheet["name"] = name.content
+        elif i == "look":
+          await message.channel.send('Descibe your appearance.')
+          look = await client.wait_for('message')
+          player_sheet['look'] = look.content
+        elif i == 'armor' or i == "hitpoints" or i == "damage":
+          player_sheet[i] = 0
+        else:
+          await message.channel.send(f"roll for your {i}")
+          roll = await client.wait_for('message')
+          dice_roll = await self.dice(roll.content, message)
+          player_sheet[i] = dice_roll
 
-      await message.channel.send('Descibe your appearance.')
-      look = await client.wait_for('message')
-
-      player_sheet['look'] = look.content
-
-      await message.channel.send('for now in character create roll without the /')
-      # armor 
-      player_sheet['armor'] = 0
-      #hitpoints
-      player_sheet['hitpoint'] = 0
-      #damage
-      player_sheet['damage'] = 0
-
-      await message.channel.send("roll for your strength")
-      roll = await client.wait_for('message')
-      dice_roll = await self.dice(roll.content, message)
-
-      player_sheet['strength'] = dice_roll
-
-      await message.channel.send("roll for your dexterity")
-      roll = await client.wait_for('message')
-      dice_roll = await self.dice(roll.content, message)
-
-      player_sheet['dexterity'] = dice_roll
-
-
-      await message.channel.send("roll for your constitution")
-      roll = await client.wait_for('message')
-      dice_roll = await self.dice(roll.content, message)
-
-      player_sheet['contitution'] = dice_roll
-
-
-      await message.channel.send("roll for your inteligence")
-      roll = await client.wait_for('message')
-      dice_roll = await self.dice(roll.content, message)
-
-      player_sheet['inteligence'] = dice_roll
-
-
-      await message.channel.send("roll for your wisdom")
-      roll = await client.wait_for('message')
-      dice_roll = await self.dice(roll.content, message)
-
-      player_sheet['wisdom'] = dice_roll
-
-
-      await message.channel.send("roll for your charisma")
-      roll = await client.wait_for('message')
-      dice_roll = await self.dice(roll.content, message)
-
-      player_sheet['chrisma'] = dice_roll
-
+      
       await message.channel.send('player sheet:')
       await message.channel.send(player_sheet.items())
-
-    
-
 
 client = MyClient()
 client.run(os.environ['TOKEN'])
